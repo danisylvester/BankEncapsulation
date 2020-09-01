e4d6b5624714561b6e69a1deec073301c05015f4
danisylvester <maria.sylvester10@gmail.com>
2020-09-01 15:01:17 -0500

    Created program with BankAccount class that has a Deposit and GetBalance method.

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var daniBankAccount = new BankAccount();
+
+            daniBankAccount.Deposit(1000);
+            Console.WriteLine(daniBankAccount.GetBalance());
+        }
+    }
+}
